bc7d1a9dd27ab6a1e038a7c5eecf13dc0f60c192
arisonreis <arisontechp@gmail.com>
2025-03-28 03:26:40 -0300

    Adiciona configuração do projeto para a aplicação de pedidos, incluindo ajustes no Docker, ESLint, TypeScript e Prisma. Cria arquivos de configuração e estrutura para o gerenciamento de pedidos, além de atualizar dependências e scripts no package.json.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ['eslint.config.mjs', 'node_modules'],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
@@ -29,7 +29,9 @@ export default tseslint.config(
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/require-await': 'off',
     },
   },
 );
